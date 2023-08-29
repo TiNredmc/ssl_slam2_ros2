51d8767f091a237c4a9ddb1d2831d150ee525d7b
TiNredmc <thipok17@gmail.com>
2023-08-29 11:26:47 +0700

    Map save at user home path
    Signed-off-by: TiNredmc <thipok17@gmail.com>

--- launch/ssl_slam2_mapping.launch.py
+++ launch/ssl_slam2_mapping.launch.py
@@ -5,19 +5,21 @@ import launch_ros.actions
 
 from ament_index_python.packages import get_package_share_directory
 
+home = os.path.expanduser('~')
+
 def generate_launch_description():
 
     laser_proc_node = launch_ros.actions.Node(
         package='ssl_slam2',
         executable='ssl_slam2_laser_processing_node',
         parameters=[
-            {'scan_period': 0.1},
+            {'scan_period': 0.05},
             {'vertical_angle': 2.0},
-            {'max_dis': 9.0},
+            {'max_dis': 5.0},
             {'map_resolution': 0.02},
-            {'min_dis': 0.1},
+            {'min_dis': 0.4},
             {'skip_frames': 1},
-            {'map_path': '/home/tinlethax/'},   
+            {'map_path': home},   
         ],
         output='screen',
     )
@@ -26,13 +28,13 @@ def generate_launch_description():
         package='ssl_slam2',
         executable='ssl_slam2_odom_estimation_mapping_node',
         parameters=[
-            {'scan_period': 0.1},
+            {'scan_period': 0.05},
             {'vertical_angle': 2.0},
-            {'max_dis': 9.0},
+            {'max_dis': 5.0},
             {'map_resolution': 0.02},
-            {'min_dis': 0.1},
+            {'min_dis': 0.4},
             {'skip_frames': 1},
-            {'map_path': '/home/tinlethax/'},   
+            {'map_path': home},   
         ],
         output='screen',
     )
@@ -41,13 +43,13 @@ def generate_launch_description():
         package='ssl_slam2',
         executable='ssl_slam2_map_optimization_node',
         parameters=[
-            {'scan_period': 0.1},
+            {'scan_period': 0.05},
             {'vertical_angle': 2.0},
-            {'max_dis': 9.0},
+            {'max_dis': 5.0},
             {'map_resolution': 0.02},
-            {'min_dis': 0.1},
+            {'min_dis': 0.4},
             {'skip_frames': 1},
-            {'map_path': '/home/tinlethax/'},
+            {'map_path': home},
             {'min_map_update_distance' : 1.0},
             {'min_map_update_angle': 30.0},
             {'min_map_update_frame': 8.0},
